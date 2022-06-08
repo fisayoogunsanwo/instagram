62ea9bbce114d885613b1df406eac04c95115656
Fisayo Ogunsanwo <90068822+fisayoogunsanwo@users.noreply.github.com>
2022-06-08 18:16:53 +0100

    initial commit

---
+++ logic.js
@@ -0,0 +1,28 @@
+// let logIn= document.getElementById("b1");
+// let username= document.getElementById("in1");
+// let password=document.getElementById("in2");
+
+
+
+let logo=document.getElementById("logo");
+
+function removeLogo(){
+    logo.style.display="none";
+    }
+
+window.onload= setTimeout(removeLogo, 1700);
+
+
+
+// window.addEventListener("load", function(){
+  
+//     logo.style.display="none";
+//     });
+
+
+// window.onload= function(){
+//     logo.style.display="none";
+// };
+
+
+
